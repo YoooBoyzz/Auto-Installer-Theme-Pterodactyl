d8e5db15337f7edca60f299926d580e9ed032570
Yooo <evostayo344@gmail.com>
2025-03-03 08:13:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ display_welcome() {
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
-  echo -e "${BLUE}[+]                  © YOOOOO0000                   [+]${NC}"
+  echo -e "${BLUE}[+]                  © Yooo - 2024                  [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${RED}[+] =============================================== [+]${NC}"
   echo -e ""
